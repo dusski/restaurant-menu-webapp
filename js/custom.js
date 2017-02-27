6e3db2907313bed5bd51f1cc4e544e84a4fced25
dusski <dusski@gmail.com>
2017-02-27 14:55:57 +0100

    još jednom isto

--- js/custom.js
+++ js/custom.js
@@ -107,7 +107,7 @@ function Naruci() {
       $('#narudzbina').append('<tr><td>' + element.proizvod + '</td><td><button class="btn btn-default " onclick="console.log(\'hello world\');" type="button" role="button">+</button>' + element.kolicina + '<button class="btn btn-default " onclick="" type="button" role="button">-</button></td></tr>');
     })
 
-    // imaš id proizvoda koji šalješ kroz narudžbinu, upotrebi to da dođeš do input number polja pa mu menjaj vrednost sa + i -
+    // imaš id proizvoda koji šalješ kroz narudžbinu, upotrebi to da dođeš do input number polja pa mu menjaj vrednost sa + i - 
   }
 
   // da resetuje vrednost polja, ali mora i da ažurira natpis i prikaže nulu
